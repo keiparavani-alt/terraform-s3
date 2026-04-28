3a45f88903b1e65198d9fdc72e0747a6211f2be6
Kei PARAVANI <kei.paravani@raiffeisen.al>
2026-04-28 15:02:54 +0200

    added roles

--- roles.tf
+++ roles.tf
@@ -1,3 +1,4 @@
+#role for read only, can list the objects in the bucket
 resource "aws_iam_policy" "s3_read_only" {
   name = "${local.final_bucket_name}-read-only"
 
@@ -17,6 +18,87 @@ resource "aws_iam_policy" "s3_read_only" {
           "s3:GetObject"
         ]
         Resource = "${aws_s3_bucket.my_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+#can write but not read or delete
+resource "aws_iam_policy" "s3_write_only" {
+  name = "${local.final_bucket_name}-write-only"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject"
+        ]
+        Resource = "${aws_s3_bucket.my_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+#read+write (no deleting)
+resource "aws_iam_policy" "s3_read_write" {
+  name = "${local.final_bucket_name}-read-write"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = aws_s3_bucket.my_bucket.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = "${aws_s3_bucket.my_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+#delete role
+resource "aws_iam_policy" "s3_delete" {
+  name = "${local.final_bucket_name}-delete"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:DeleteObject"
+        ]
+        Resource = "${aws_s3_bucket.my_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+#admin role
+resource "aws_iam_policy" "s3_admin" {
+  name = "${local.final_bucket_name}-admin"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "s3:*"
+        Resource = [
+          aws_s3_bucket.my_bucket.arn,
+          "${aws_s3_bucket.my_bucket.arn}/*"
+        ]
       }
     ]
   })
